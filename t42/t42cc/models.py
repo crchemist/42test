e9083b06c32940da3def17daceac9b749a14f898
Mykola Kharechko <crchemist@gmail.com>
2010-08-30 20:29:04 +0300

    ticket:3 added yet two fields to RequestModel

--- t42/t42cc/models.py
+++ t42/t42cc/models.py
@@ -17,3 +17,5 @@ class RequestModel(models.Model):
     """Store path of each request
     """
     path = models.CharField(max_length=256)
+    username = models.CharField(max_length=256, blank=True)
+    method = models.CharField(max_length=256)
