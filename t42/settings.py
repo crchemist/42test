701706db32e65da4553427b32b67a0122209178d
Mykola Kharechko <crchemist@gmail.com>
2010-09-01 22:33:25 +0300

    added stolen styles

--- t42/settings.py
+++ t42/settings.py
@@ -1,5 +1,8 @@
 # Django settings for t42 project.
 import django.conf.global_settings as DEFAULT_SETTINGS
+import os
+
+PROJECT_PATH = os.path.realpath(os.path.dirname(__file__))
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -46,7 +49,7 @@ USE_L10N = True
 
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = os.path.join(PROJECT_PATH, 'static')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
--- t42/urls.py
+++ t42/urls.py
@@ -3,6 +3,8 @@ from django.conf.urls.defaults import *
 from django.contrib import admin
 admin.autodiscover()
 
+import settings
+
 urlpatterns = patterns('',
     # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
     # to INSTALLED_APPS to enable admin documentation:
@@ -10,5 +12,8 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     (r'^admin/', include(admin.site.urls)),
+    (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root':
+       settings.MEDIA_ROOT, 'show_indexes': True}),
+
     (r'', include('t42cc.urls')),
 )
